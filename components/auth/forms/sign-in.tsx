d6a40ce0ceaf51ff3a479556a448157585f73377
Paulo Silva <pauloalexandreduartesilva@gmail.com>
2024-11-22 22:09:25 +0000

    fix: layout shift in landing page due to Cloudflare Turnstile (#25)

--- components/auth/forms/sign-in.tsx
+++ components/auth/forms/sign-in.tsx
@@ -85,10 +85,17 @@ export const SignInForm = () => {
                   type="password"
                 />
               </div>
-              <div className="text-center">
+              <div className="flex min-h-[140px] justify-center sm:min-h-[72px]">
                 <Turnstile
-                  siteKey={env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
+                  className={`hidden sm:block`}
                   onSuccess={setCaptchaToken}
+                  siteKey={env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
+                />
+                <Turnstile
+                  className={`block sm:hidden`}
+                  onSuccess={setCaptchaToken}
+                  options={{ size: "compact" }}
+                  siteKey={env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
                 />
               </div>
               <Button className="w-full" disabled={isPending} type="submit">
